1da22571539b8bce1737cc32f0675aa48df1f4dd
Divyam Singh <divyamsingh1918@gmail.com>
2025-01-20 12:29:50 +0530

    Update Server.js

--- backend/Server.js
+++ backend/Server.js
@@ -1,6 +1,9 @@
 import express from "express";
+import connectDB from "./config/mongodb.js";
+
 
 const app = express()
 const port = process.env.PORT || 4000
+connectDB();
 
 app.listen(port, ()=>(console.log('Server started on PORT '+port)))
