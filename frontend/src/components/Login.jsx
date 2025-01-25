6da96012b6f3373564b4c58f70ee05b6202df9d0
Divyam Singh <divyamsingh1918@gmail.com>
2025-01-25 13:05:37 +0530

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import axios from 'axios'
 import { backendUrl } from '../App'
 import { toast } from 'react-toastify'
+import { useEffect } from 'react'
 const Login = ({setToken}) => {
   const [email,setEmail] = useState('')
   const [password,setPassword] = useState('')
@@ -22,8 +23,16 @@ const Login = ({setToken}) => {
       toast.error(error.message)
     }
   }
+  useEffect(() => {
+    
+    document.body.style.overflow = "hidden";
+
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, []);
   return (
-    <div className='min-h-screen flex items-center justify-center w-full'>
+    <div className='overflow-hidden min-h-screen flex items-center justify-center w-full'>
       <div className='bg-white shadow-md rounded-lg px-8 py-6 max-w-md'>
         <h1 className='text-xl font-bold mb-4'>Login to your account</h1>
         <form onSubmit={onSubmitHandler}>
