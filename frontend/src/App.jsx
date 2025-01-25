b13661784d7e4d0c04a4c48529a0e1200efabfc9
Divyam Singh <divyamsingh1918@gmail.com>
2025-01-25 11:27:43 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 
-
+import Login from './components/Login'
 import './App.css'
-
+export const backendUrl=import.meta.env.VITE_BACKEND_URL
 function App() {
  
 
   return (
     <div className='sm:w-full h-full'>
+      <Login/>
      <div className='bg-white  text-blue-500'> Hello </div>
     </div>
   )
