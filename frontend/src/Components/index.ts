07ce54cd2bec4a9acf863c052515cc030281da02
Divyam Singh <divyamsingh1918@gmail.com>
2025-01-27 22:47:58 +0530

    Updates

---
+++ frontend/src/Components/index.ts
@@ -0,0 +1,8 @@
+import Button from "./Button";
+import Signup from "./Signup";
+
+export {
+    Button,
+    Signup
+
+}
---
+++ frontend/src/Pages/Signup.tsx
@@ -0,0 +1,15 @@
+import {Signup as SignupComponent} from "../Components/index";
+
+
+import React from 'react'
+
+const Signup = () => {
+
+  return (
+    <div>
+      <SignupComponent/>
+    </div>
+  )
+}
+
+export default Signup
