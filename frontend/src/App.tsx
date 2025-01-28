70e113a1f4f2758476256a02b5e8efaf730b23a0
Divyam Singh <divyamsingh1918@gmail.com>
2025-01-28 21:32:14 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,31 @@
+import { useEffect, useState } from 'react'
 import './App.css'
-import {  Chatbox as ChatboxComponent } from './Components/index'
+import {  About } from './Components/index'
 import {SplashScreen as SplashScreenComponent} from './Components/index'
 
 function App() { 
+  const [loading,setLoading] = useState(true)
+  useEffect(()=>{
+    const timer = setTimeout(() => {
+      setLoading(false); // Hide splash screen after the timer
+    }, 2000); // Example: 2 seconds splash duration
+
+    return () => clearTimeout(timer); // Cleanup timer
+
+  })
   return (
     < >
-      
-      <SplashScreenComponent onAnimationEnd={function (): void {throw new Error('Function not implemented.') } }/>
+       {loading ? (
+         <SplashScreenComponent onAnimationEnd={function (): void {throw new Error('Function not implemented.') } }/> // Show SplashScreen while loading is true
+      ) : (
+        <>
+          
+        </>
+      )}
+     
     </>
   )
 }
 
 export default App
+ 
