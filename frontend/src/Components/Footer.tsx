b23e3ae7fec1efd0f9db4d9571941b949edea198
focusedaf <126765236+focusedaf@users.noreply.github.com>
2025-02-01 15:54:52 +0530

    update footer

--- frontend/src/Components/Footer.tsx
+++ frontend/src/Components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="fixed bottom-0 left-0 right-0 w-full p-4 z-50">
+    <div className="fixed bottom-0 left-0 right-0 w-full  z-50">
       <footer className="bg-black w-screen rounded-xl">
         <div className="w-screen p-4 md:py-8 text-center">
           <span className="block text-sm text-gray-500 sm:text-center">
