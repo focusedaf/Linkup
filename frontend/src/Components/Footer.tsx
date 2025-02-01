74af0cedcbb8222fe0e88173e60bdb22ccbaed2a
focusedaf <126765236+focusedaf@users.noreply.github.com>
2025-02-01 13:47:22 +0530

    create Navbar

--- frontend/src/Components/Footer.tsx
+++ frontend/src/Components/Footer.tsx
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
     <div className="fixed bottom-0 left-0 right-0 w-full p-4 z-50">
-      <footer className="bg-black w-full rounded-lg">
-        <div className="w-full p-4 md:py-8 text-center">
+      <footer className="bg-black w-screen rounded-xl">
+        <div className="w-screen p-4 md:py-8 text-center">
           <span className="block text-sm text-gray-500 sm:text-center">
             © 2025{" "}
             <a href="#" className="hover:underline">
