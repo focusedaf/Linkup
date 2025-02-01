39d3db384bf20022ec09bd2318d15697fae1ccef
Divyam Singh <divyamsingh1918@gmail.com>
2025-02-01 13:25:43 +0530

    Update Signup.tsx

--- frontend/src/Pages/Signup.tsx
+++ frontend/src/Pages/Signup.tsx
@@ -1,22 +1,25 @@
 import { useEffect } from "react"
 import { LockClosedIcon, EnvelopeIcon, UserIcon } from "@heroicons/react/24/solid";
+import {FcGoogle } from 'react-icons/fc'
+import {FaGithub} from 'react-icons/fa'
+ 
 
 const Signup = () => {
-    // its a react hook
+     
     useEffect(() => {
-        // disables scrolling on the page
+         
         document.body.style.overflow = "hidden";
     
         return () => {
-          // re-enables scrolling when the component unmounts
+           
           document.body.style.overflow = "auto";
         };
-      }, []);  // empty dependency array means this effect runs only on mount/unmount
+      }, []);   
 
   return (
-    <div className="absolute top-0 left-0 flex min-h-screen w-screen justify-center items-center bg-[radial-gradient(circle_at_bottom,_var(--tw-gradient-stops))] from-gray-300 to-blue-900 ">
-      <div className="bg-[#121826] max-w-[700px] text-center p-10 rounded-xl shadow-2xl">
-        <h2 className="text-white text-3xl font-bold mb-2 font-serif ">
+    <div className="absolute top-0 left-0 flex min-h-screen w-screen justify-center items-center bg-[radial-gradient(500px_circle_at_bottom,_var(--tw-gradient-stops))] from-gradient via-mid to-background">
+      <div className=" backdrop-blur-md md:max-w-[1000px] sm:max-w-[7000px] text-center p-10 rounded-xl md:border border-white shadow-2xl">
+        <h2 className="text-white text-3xl font-bold mb-2 ">
           Create an account
         </h2>
         <p className="mb-5  text-sm">
@@ -27,14 +30,24 @@ const Signup = () => {
         </p>
 
         <form method="#" action="#" className="flex flex-col">
-          <div className="relative">
+          <div className="relative flex justify-between">
+             
             <UserIcon className="w-6 h-6 absolute left-3 top-1/3 transform -translate-y-1/2 " />
             <input
               type="text"
-              placeholder="Name"
-              className="p-3 pl-15 mb-4 w-full rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400 border border-[#1B2A49] "
+              placeholder="First Name"
+              className="p-3 pl-15 mb-4 w-full rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400 border border-gray-400 "
               required
             />
+           
+            
+            <input
+              type="text"
+              placeholder="Last Name"
+              className="p-3 pl-15 mb-4 w-full rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400 border border-gray-400 "
+              required
+            />
+             
           </div>
 
           <div className="relative">
@@ -42,8 +55,16 @@ const Signup = () => {
             <input
               type="email"
               placeholder="Email"
-              className="p-3 pl-15 mb-4 w-full rounded-md  focus:outline-none focus:ring-2 focus:ring-blue-400 border border-[#1B2A49] "
+              className="p-3 pl-15 mb-4 w-full rounded-md  focus:outline-none focus:ring-2 focus:ring-blue-400 border border-gray-400 "
               required
+            />
+          </div>
+          <div className="relative">
+            <EnvelopeIcon className="w-6 h-6 absolute left-3 top-1/3 transform -translate-y-1/2  " />
+            <input
+              type="text"
+              placeholder="Username"
+              className="p-3 pl-15 mb-4 w-full rounded-md  focus:outline-none focus:ring-2 focus:ring-blue-400 border border-gray-400"
             />
           </div>
 
@@ -52,10 +73,11 @@ const Signup = () => {
             <input
               type="password"
               placeholder="Password"
-              className="p-3 pl-15  mb-4 w-full rounded-md  focus:outline-none focus:ring-2 focus:ring-blue-400 border border-[#1B2A49] "
+              className="p-3 pl-15  mb-4 w-full rounded-md  focus:outline-none focus:ring-2 focus:ring-blue-400 border border-gray-400 "
               required
             />
           </div>
+          
 
           <div className="flex justify-center mt-2 mb-4 text-sm">
             <input
@@ -72,12 +94,26 @@ const Signup = () => {
               </a>
             </p>
           </div>
-
-          <button className=" mt-4 p-3 w-full cursor-pointer font-semibold bg-gradient-to-r from-[#3A5BBC] to-[#4F72D4] hover:scale-105 hover:brightness-110 rounded-lg shadow-lg transition duration-300 ">
+          <div className=" w-full relative">
+            <FcGoogle className="w-6 h-6 absolute left-3 top-1/3 transform -translate-y-1/2 " />
+            <button
+              
+              className="p-3 pl-15  mb-4 w-full rounded-md  focus:outline-none focus:ring-2 focus:ring-blue-400 border border-gray-400 "> Signup With Google
+              
+           </button>
+          </div>
+          <div className="relative">
+            <FaGithub className="w-6 h-6 absolute left-3 top-1/3 transform -translate-y-1/2 " />
+            <button className="p-3 pl-15  mb-4 w-full rounded-md  focus:outline-none focus:ring-2 focus:ring-blue-400 border border-gray-400 "> Signup With Github</button>
+          </div>
+          <div className="flex items-center justify-center"> 
+          <button className="  mt-4 p-3 w-1/3 cursor-pointer font-semibold bg-gradient-to-r from-blue-500 to-blue-700 hover:scale-105 hover:brightness-110 rounded-lg shadow-lg transition duration-300 ">
             Sign up
           </button>
+          </div>
         </form>
       </div>
+       
     </div>
   );
 }
