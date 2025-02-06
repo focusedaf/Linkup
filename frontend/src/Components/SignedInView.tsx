b1e2000f549ba96af007dbc61d5ebfbee35c7bea
focusedaf <126765236+focusedaf@users.noreply.github.com>
2025-02-07 01:25:37 +0530

    add gradient and icons

--- frontend/src/Components/SignedInView.tsx
+++ frontend/src/Components/SignedInView.tsx
@@ -1,31 +1,30 @@
 import { VscArrowRight } from "react-icons/vsc";
-import { FaPhoneAlt } from "react-icons/fa";
+import { FaPhoneAlt, FaSearch, FaComment, FaEllipsisV } from "react-icons/fa";
 import { FiUpload } from "react-icons/fi";
-import { FaSearch } from "react-icons/fa";
 
 const SignedInView = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen w-full ">
-      <div className="flex flex-col text-center ">
+    <div className="absolute top-0 left-0 flex flex-col min-h-screen w-screen justify-center items-center bg-[radial-gradient(500px_circle_at_bottom,_var(--tw-gradient-stops))] from-gradient via-mid to-background">
+      <div className="flex flex-col text-center">
         <div className="font-bold text-3xl space-y-3">
           <p className="text-white">
-            Welcome back <span className="text-blue-700">user</span> !
+            Welcome back <span className="text-blue-700">user</span>!
           </p>
           <p className="text-white">
-            Ready to <span className="text-blue-700">connect</span> ?
+            Ready to <span className="text-blue-700">connect</span>?
           </p>
           <p className="text-white">
-            Start a <span className="text-blue-700">call</span> now !!
+            Start a <span className="text-blue-700">call</span> now!
           </p>
         </div>
 
         <div className="flex flex-col sm:flex-row gap-10 mt-6">
-          <button className="bg-blue-700 text-white rounded-xl px-6 py-3 pr-12 flex items-center relative hover:bg-blue-800 transition cursor-pointer">
+          <button className="bg-blue-700 text-white rounded-xl px-4 py-2 pr-10 flex items-center relative hover:bg-blue-800 transition cursor-pointer">
             <VscArrowRight className="w-6 h-6 absolute right-3 top-1/2 transform -translate-y-1/2" />
             Start A Call
           </button>
 
-          <button className="bg-blue-700 text-white rounded-xl px-6 py-3 pr-12 flex items-center relative hover:bg-blue-800 transition cursor-pointer">
+          <button className="bg-blue-700 text-white rounded-xl px-4 py-2 pr-10 flex items-center relative hover:bg-blue-800 transition cursor-pointer">
             <VscArrowRight className="w-6 h-6 absolute right-3 top-1/2 transform -translate-y-1/2" />
             Schedule A Call
           </button>
@@ -33,62 +32,124 @@ const SignedInView = () => {
       </div>
 
       {/* features */}
-      <div className="flex flex-col sm:flex-row gap-10 mt-10 bg-inherit">
-        {/* chat */}
-        <div className="flex gap-4 justify-center items-center backdrop-blur-xl bg-gray/50 rounded-xl">
-          <p className="text-white font-semibold">Chat </p>
-          <button className="bg-blue-700 text-white rounded-xl px-4 py-2  hover:bg-blue-800 transition cursor-pointer">
-            Create DM
-          </button>
+      <div className="flex flex-col sm:flex-row justify-evenly items-center w-full ">
+        {/* Chat */}
+        <div className="flex flex-col gap-5 backdrop-blur-xl bg-gray/50 rounded-xl p-5">
+          <div className="flex gap-7 justify-center items-center">
+            <p className="text-white font-semibold">Chat</p>
+            <button className="bg-blue-700 text-white rounded-xl px-4 py-2 hover:bg-blue-800 transition cursor-pointer">
+              Create DM
+            </button>
+          </div>
+          <div className="flex flex-col gap-3">
+            <div className="text-white relative pl-10">
+              <div className="flex w-7 h-7 rounded-full items-center justify-center bg-amber-200 absolute left-0">
+                1
+              </div>
+              john
+            </div>
+            <div className="text-white relative pl-10">
+              <div className="flex w-7 h-7 rounded-full items-center justify-center bg-amber-200 absolute left-0">
+                1
+              </div>
+              jane
+            </div>
+            <div className="text-white relative pl-10">
+              <div className="flex w-7 h-7 rounded-full items-center justify-center bg-amber-200 absolute left-0">
+                1
+              </div>
+              mia
+            </div>
+            <div className="text-white relative pl-10">
+              <div className="flex w-7 h-7 rounded-full items-center justify-center bg-amber-200 absolute left-0">
+                1
+              </div>
+              peter
+            </div>
+          </div>
         </div>
 
         {/* friends */}
-        <div className="flex gap-4 justify-center items-center backdrop-blur-xl bg-gray/50 rounded-xl">
-          <p className="text-white font-semibold">Friends</p>
-          <div className="backdrop-blur-xl bg-gray/50 rounded-xl border border-gray-100 pr-15 m-5 relative ">
-            <FaSearch className="w-4 h-4 absolute right-2 top-1/2 transform -translate-y-1/2" />
-            Search
+        <div className="flex flex-col gap-5 backdrop-blur-xl bg-gray/50 rounded-xl p-5">
+          <div className="flex gap-7 items-center">
+            <p className="text-white font-semibold">Friends</p>
+            <div className="backdrop-blur-xl bg-gray/50 rounded-xl border border-gray-100 pr-20 p-1 m-5 relative ">
+              <FaSearch className="w-4 h-4 absolute right-2 top-1/2 transform -translate-y-1/2" />
+              Search
+            </div>
+
+            <button className="bg-blue-700 text-white rounded-xl px-4 py-2 hover:bg-blue-800 transition cursor-pointer">
+              Add Friend
+            </button>
           </div>
-          <button className="bg-blue-700 text-white rounded-xl px-4 py-2  hover:bg-blue-800 transition cursor-pointer">
-            Add Friend
-          </button>
+          <div className="flex flex-col gap-3">
+            <div className="text-white relative pl-10">
+              <div className="flex w-7 h-7 rounded-full items-center justify-center bg-amber-200 absolute left-0">
+                1
+              </div>
+              john
+              <FaComment className="w-4 h-4 absolute right-10 top-1/2 transform -translate-y-1/2" />
+              <FaEllipsisV className="w-4 h-4 absolute right-3 top-1/2 transform -translate-y-1/2" />
+            </div>
+            <div className="text-white relative pl-10">
+              <div className="flex w-7 h-7 rounded-full items-center justify-center bg-amber-200 absolute left-0">
+                1
+              </div>
+              jane
+              <FaComment className="w-4 h-4 absolute right-10 top-1/2 transform -translate-y-1/2" />
+              <FaEllipsisV className="w-4 h-4 absolute right-3 top-1/2 transform -translate-y-1/2" />
+            </div>
+            <div className="text-white relative pl-10">
+              <div className="flex w-7 h-7 rounded-full items-center justify-center bg-amber-200 absolute left-0">
+                1
+              </div>
+              mia
+              <FaComment className="w-4 h-4 absolute right-10 top-1/2 transform -translate-y-1/2" />
+              <FaEllipsisV className="w-4 h-4 absolute right-3 top-1/2 transform -translate-y-1/2" />
+            </div>
+            <div className="text-white relative pl-10">
+              <div className="flex w-7 h-7 rounded-full items-center justify-center bg-amber-200 absolute left-0">
+                1
+              </div>
+              peter
+              <FaComment className="w-4 h-4 absolute right-10 top-1/2 transform -translate-y-1/2" />
+              <FaEllipsisV className="w-4 h-4 absolute right-3 top-1/2 transform -translate-y-1/2" />
+            </div>
+          </div>
         </div>
       </div>
 
-      <div className="flex flex-col sm:flex-row gap-10 mt-10 bg-inherit">
+      <div className="flex justify-evenly items-center w-full">
         {/* vibes */}
-        <div className="flex flex-col gap-4 justify-center items-center backdrop-blur-xl bg-gray/50 rounded-xl">
+        <div className="flex flex-col gap-4 backdrop-blur-xl bg-gray/50 rounded-xl p-5">
           <p className="text-white font-semibold">Vibes</p>
-          <button className=" pr-10 flex items-center relative">
+          <button className="pr-10 flex items-center relative">
             <FiUpload className="w-4 h-4 absolute right-2 top-1/2 transform -translate-y-1/2" />
             Themes
           </button>
-          <button className=" pr-10 flex items-center relative">
-            {" "}
+          <button className="pr-10 flex items-center relative">
             <FiUpload className="w-4 h-4 absolute right-2 top-1/2 transform -translate-y-1/2" />
             Status
           </button>
-          <button className=" pr-10 flex items-center relative">
-            {" "}
+          <button className="pr-10 flex items-center relative">
             <FiUpload className="w-4 h-4 absolute right-2 top-1/2 transform -translate-y-1/2" />
             Profile Music
           </button>
-          <button className=" pr-10 flex items-center relative">
-            {" "}
+          <button className="pr-10 flex items-center relative">
             <FiUpload className="w-4 h-4 absolute right-2 top-1/2 transform -translate-y-1/2" />
             Virtual Background
           </button>
         </div>
 
         {/* events */}
-        <div className="flex flex-col gap-4 justify-center items-center backdrop-blur-xl bg-gray/50 rounded-xl ">
+        <div className="flex flex-col gap-4 backdrop-blur-xl bg-gray/50 rounded-xl p-5 ">
           <p className="text-white font-semibold">Events</p>
-          <button className=" pr-10 flex items-center relative">
+          <button className="pr-10 flex items-center relative">
             <FaPhoneAlt className="w-4 h-4 absolute right-2 top-1/2 transform -translate-y-1/2" />
             Upcoming Calls
           </button>
           <button className="pr-10 flex items-center relative">
-            <FaPhoneAlt className="w-4 h-4 absolute right-2 transform-translate-y-1/2" />
+            <FaPhoneAlt className="w-4 h-4 absolute right-2 top-1/2 transform -translate-y-1/2" />
             Schedule A Call
           </button>
         </div>
@@ -98,3 +159,39 @@ const SignedInView = () => {
 };
 
 export default SignedInView;
+
+// <div className="flex justify-evenly items-center w-full">
+//   {/* vibes */}
+//   <div className="flex flex-col gap-4 backdrop-blur-xl bg-gray/50 rounded-xl p-5">
+//     <p className="text-white font-semibold">Vibes</p>
+//     <button className="pr-10 flex items-center relative">
+//       <FiUpload className="w-4 h-4 absolute right-2 top-1/2 transform -translate-y-1/2" />
+//       Themes
+//     </button>
+//     <button className="pr-10 flex items-center relative">
+//       <FiUpload className="w-4 h-4 absolute right-2 top-1/2 transform -translate-y-1/2" />
+//       Status
+//     </button>
+//     <button className="pr-10 flex items-center relative">
+//       <FiUpload className="w-4 h-4 absolute right-2 top-1/2 transform -translate-y-1/2" />
+//       Profile Music
+//     </button>
+//     <button className="pr-10 flex items-center relative">
+//       <FiUpload className="w-4 h-4 absolute right-2 top-1/2 transform -translate-y-1/2" />
+//       Virtual Background
+//     </button>
+//   </div>
+
+//   {/* events */}
+//   <div className="flex flex-col gap-4 backdrop-blur-xl bg-gray/50 rounded-xl p-5 ">
+//     <p className="text-white font-semibold">Events</p>
+//     <button className="pr-10 flex items-center relative">
+//       <FaPhoneAlt className="w-4 h-4 absolute right-2 top-1/2 transform -translate-y-1/2" />
+//       Upcoming Calls
+//     </button>
+//     <button className="pr-10 flex items-center relative">
+//       <FaPhoneAlt className="w-4 h-4 absolute right-2 top-1/2 transform -translate-y-1/2" />
+//       Schedule A Call
+//     </button>
+//   </div>
+// </div>;
