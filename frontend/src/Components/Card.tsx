4e93dd6d2359e5477305854d856699a6e941a7db
focusedaf <126765236+focusedaf@users.noreply.github.com>
2025-02-13 16:43:33 +0530

    add more stuff to card

--- frontend/src/Components/Card.tsx
+++ frontend/src/Components/Card.tsx
@@ -9,9 +9,9 @@ const Card = ({label,className,onClick}:input) => {
     <div
       onClick={onClick}
       className={`mt-10 mx-auto max-w-md overflow-hidden rounded-xl shadow-md md:max-w-2xl cursor-pointer p-[1.5px] animate-border-rotate bg-[conic-gradient(from_var(--border-angle)_at_50%_50%,var(--tw-gradient-stops))] 
-     from-sky-300 via-blue-800 to-blue-900 ${className}`}
+     from-sky-300 via-blue-800 to-blue-900 hover:translate-x-5  ${className}`}
     >
-      <div className=" w-full h-full rounded-xl px-100 py-60 pr-2 bg-[radial-gradient(350px_circle_at_top,_var(--tw-gradient-stops))] from-gradient via-mid to-background">
+      <div className=" w-full h-full rounded-xl px-100 py-60 pr-2 bg-[radial-gradient(350px_circle_at_top,_var(--tw-gradient-stops))] from-gradient via-mid to-background hover:bg-[radial-gradient(350px_circle_at_bottom,_var(--tw-gradient-stops))] from-mid via-gradient to-background ">
         {label}
       </div>
     </div>
