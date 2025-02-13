8614aabb5a95cbe0df6d3ceabdfa8cad214e6220
focusedaf <126765236+focusedaf@users.noreply.github.com>
2025-02-13 16:28:16 +0530

    update card

--- frontend/src/Components/Card.tsx
+++ frontend/src/Components/Card.tsx
@@ -8,7 +8,8 @@ const Card = ({label,className,onClick}:input) => {
   return (
     <div
       onClick={onClick}
-      className={`mt-10 mx-auto max-w-md overflow-hidden rounded-xl shadow-md md:max-w-2xl cursor-pointer p-[1.5px] bg-gradient-to-r from-sky-300 via-blue-800 to-blue-900 ${className}`}
+      className={`mt-10 mx-auto max-w-md overflow-hidden rounded-xl shadow-md md:max-w-2xl cursor-pointer p-[1.5px] animate-border-rotate bg-[conic-gradient(from_var(--border-angle)_at_50%_50%,var(--tw-gradient-stops))] 
+     from-sky-300 via-blue-800 to-blue-900 ${className}`}
     >
       <div className=" w-full h-full rounded-xl px-100 py-60 pr-2 bg-[radial-gradient(350px_circle_at_top,_var(--tw-gradient-stops))] from-gradient via-mid to-background">
         {label}
